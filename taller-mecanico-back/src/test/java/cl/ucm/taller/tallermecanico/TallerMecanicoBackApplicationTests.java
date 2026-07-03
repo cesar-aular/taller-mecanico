f4ba002de4182e3bfdf24239db9fbe855c3322a7
César Aular <cesaraular02@gmail.com>
2026-07-03 00:26:55 -0400

    Commit Inicial: Creación de proyecto back con sprinboot en intelliJ IDEA usando maven; con las dependencias Spring dev tools, Lombok, Validation, Spring Web PosgreSQL Driver, Spring Data JPA. Creación de docker-compose para inicializar la base de datos en un contenedor con PostgreSQL, con el nombre taller_mecanico_db, con la base de datos taller_db; usuario: admin, password: password123; disponibilizado en el puerto 5432

---
+++ taller-mecanico-back/src/test/java/cl/ucm/taller/tallermecanico/TallerMecanicoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ucm.taller.tallermecanico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerMecanicoBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
